d7066795705eb97b07afd9db191744d84874230b
Harm <mail@harmtemolder.com>
2020-09-25 09:00:59 -0300

    Rewrite to handle conda environment.yml files

--- README.md
+++ README.md
@@ -1,35 +1,35 @@
-#### AutoVenv
-[Oh My Fish][omf] plugin to automatically activate/deactivate Python virtual enviroments upon entering/leaving a directory.
+# `autoconda`
 
-[![MIT License][license-badge]](/LICENSE)
-</br>
+Fish shell plugin to automatically activate/deactivate Python virtual enviroments upon entering/leaving a directory.
+
+[![MIT License](https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square)](/LICENSE)
 
 ## Install
-Installation with [Oh My Fish][omf]:
 
-    omf install autovenv
+Installation with [`fisher`](https://github.com/jorgebucaran/fisher):
+
+  ```shell
+  fisher add harmtemolder/fish-autoconda
+  ```
+
+(The original plugin was for [`omf`](https://github.com/oh-my-fish/oh-my-fish).)
 
 ## About
-Do you like the way Pyenv automatically switches between enviroments when you change directories and wish
-you could do the same thing with standard Python 3 venvs? Well, now you can! No complicated scripts,
-binaries or overhead needed; AutoVenv is a single file, pure `fish` solution.
+
+[Timothy Brown](https://github.com/timothybrown) wrote [an excellent plugin to switch environments with Python 3 venvs](https://github.com/timothybrown/fish-autovenv). I forked that plugin and rewrote it to handle conda environments instead.
 
 ## Usage
-Upon entering a directory that contains a Python venv (or any directory *above* it) AutoVenv will automatically
-activate it for you. Likewise, when moving *below* the venv's parent directory AutoVenv will deactivate it!
-AutoVenv can also handle cases where you move directly from one venv directory to another.
+
+Upon entering a directory that contains an `environment.yml` file—or any directory below it—`autoconda` will automatically take the value of `prefix` from it and activate that  for you. Likewise, when moving outside the environment's root directory `autoconda` will deactivate it! `autoconda` can also handle cases where you move directly from one environment directory to another.
 
 ## Settings
-    set -U autovenv_enable yes|no
-Enables/disables autovenv functionality.
 
-    set -U autovenv_announce yes|no
-Controls whether or not a message is printed when entering/leaving/changing venvs.
+<code>set -U autoconda_enable <b>yes</b>/no</code>:
+  Enables/disables `autoconda` functionality.
+<code>set -U autoconda_announce <b>yes</b>/no</code>:
+  Controls whether or not a message is printed when entering/leaving/changing environments.
+<code>set -U autoconda_debug yes/<b>no</b></code>:
+  Adds extreme verbosity for debugging.
 
 ## License
-[MIT][mit] © [Timothy Brown][author]
-
-[author]: https://github.com/timothybrown
-[license-badge]: https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square
-[mit]: http://opensource.org/licenses/MIT
-[omf]: https://github.com/oh-my-fish/oh-my-fish
+[MIT](http://opensource.org/licenses/MIT) © [Harm te Molder](https://github.com/harmtemolder)
--- init.fish
+++ init.fish
@@ -1,65 +1,125 @@
 ################################################################################
+############  AutoConda for Fish Shell, based on                    ############
 ############  AutoVenv for Fish Shell v2019.03.28 by @TimothyBrown  ############
 ################################################################################
-## AutoVenv Settings
+
+## AutoConda Settings
 if status is-interactive
-    test -z "$autovenv_announce"
-        and set -g autovenv_announce "yes"
-    test -z "$autovenv_enable"
-        and set -g autovenv_enable "yes"
+  test -z "$autoconda_announce"
+    and set -g autoconda_announce "yes"
+  test -z "$autoconda_enable"
+    and set -g autoconda_enable "yes"
+  test -z "$autoconda_debug"
+    and set -g autoconda_debug "no"
 end
 
-## AutoVenv Function
+## AutoConda Function
 # Activates on directory changes.
-function autovenv --on-variable PWD -d "Automatic activation of Python virtual environments"
-    # Check for the enable flag and make sure we're running interactive, if not return.
-    test ! "$autovenv_enable" = "yes"
-        or not status is-interactive
-        and return
-    # We start by splitting our CWD path into individual elements and iterating over each element.
-    # If our CWD is `/opt/my/hovercraft/eels` we split it into a variable containing 4 entries:
-    # `opt`, `my`, `hovercraft` and `eels`. We then build back up the path by iterating over the list
-    # and adding each element to the previous one. (We start with `/opt`, then `/opt/my` and so on.)
-    # During each run through the loop we test for a sub-directory/file called `bin/activate.fish`. If a
-    # venv is found we go ahead and break out of the loop, otherwise continue. We go through all of this
-    # instead of just checking the CWD to handle cases where the user moves into a sub-directory of the venv.
-    for _dir in (string split -n '/' "$PWD")
-        set -l _tree "$_tree/$_dir"
-        if test -e "$_tree/bin/activate.fish"
-            set _source "$_tree/bin/activate.fish"
-            if test "$autovenv_announce" = "yes"
-                set -g __autovenv_old $__autovenv_new
-                set -g __autovenv_new (basename $_tree)
-            end
-            break
-        end
+function autoconda --on-variable PWD -d "Automatic activation of Python virtual environments"
+
+  if test "$autoconda_debug" = "yes"; echo "Started autoconda"; end
+  if test "$autoconda_debug" = "yes"; echo "__autoconda_old = $__autoconda_old"; end
+  if test "$autoconda_debug" = "yes"; echo "__autoconda_new = $__autoconda_new"; end
+
+  # Check for the enable flag and make sure we're running interactive, if not return.
+  test ! "$autoconda_enable" = "yes"
+    or not status is-interactive
+    and return
+
+  # We start by splitting our CWD path into individual elements and iterating over each element.
+  # If our CWD is `/opt/my/hovercraft/eels` we split it into a variable containing 4 entries:
+  # `opt`, `my`, `hovercraft` and `eels`. We then build back up the path by iterating over the list
+  # and adding each element to the previous one. (We start with `/opt`, then `/opt/my` and so on.)
+  # During each run through the loop we test for a file called `environment.yml`. If a
+  # conda environment is found we go ahead and break out of the loop, otherwise continue. We go through all of this
+  # instead of just checking the CWD to handle cases where the user moves into a sub-directory of the conda environment.
+
+  for _dir in (string split -n '/' "$PWD")
+    set -l _tree "$_tree/$_dir"
+
+    if test "$autoconda_debug" = "yes"; echo "_tree = $_tree"; end
+
+    if test -e "$_tree/environment.yml"
+      set _source "$_tree/environment.yml"
+
+      if test "$autoconda_debug" = "yes"; echo "_source = $_source"; end
+
+      set -g __autoconda_old $__autoconda_new
+      set -xg __autoconda_new $_tree  # Export this for future runs
+
+      if test "$autoconda_debug" = "yes"; echo "__autoconda_old = $__autoconda_old"; end
+      if test "$autoconda_debug" = "yes"; echo "__autoconda_new = $__autoconda_new"; end
+
+      break
     end
-    # If we're *not* in an active venv and the venv source dir exists we activate it and return.
-    if test -z "$VIRTUAL_ENV" -a -e "$_source"
-        source "$_source"
-        if test "$autovenv_announce" = "yes"
-            echo "Activated Virtual Environment ($__autovenv_new)"
-        end
-    # Next we check to see if we're already in an active venv. If so we proceed with further tests.
-    else if test -n "$VIRTUAL_ENV"
-        # Check to see if our CWD is inside the venv directory.
-        set _dir (string match -n "$VIRTUAL_ENV*" "$PWD")
-        # If we're no longer inside the venv dirctory deactivate it and return.
-        if test -z "$_dir" -a ! -e "$_source"
-            deactivate
-            if test "$autovenv_announce" = "yes"
-                echo "Deactivated Virtual Enviroment ($__autovenv_new)"
-                set -e __autovenv_new
-                set -e __autovenv_old
-            end
-        # If we've switched into a different venv directory, deactivate the old and activate the new.
-        else if test -z "$_dir" -a -e "$_source"
-            deactivate
-            source "$_source"
-            if test "$autovenv_announce" = "yes"
-                echo "Switched Virtual Environments ($__autovenv_old => $__autovenv_new)"
-            end
-        end
+  end
+
+  # If we're *not* in an active conda environment and the conda environment source dir exists we activate it and return.
+  if test -z "$CONDA_DEFAULT_ENV" -a -e "$_source"
+
+    if test "$autoconda_debug" = "yes"; echo "_source, but no CONDA_DEFAULT_ENV"; end
+
+    __autoconda_activate_from_file "$_source"
+
+    if test "$autoconda_announce" = "yes"
+      echo "Activated Virtual Environment ($__autoconda_new)"
     end
+
+  # Next we check to see if we're already in an active conda environment. If so we proceed with further tests.
+  else if test -n "$CONDA_DEFAULT_ENV"
+
+    if test "$autoconda_debug" = "yes"; echo "CONDA_DEFAULT_ENV = $CONDA_DEFAULT_ENV"; end
+    if test "$autoconda_debug" = "yes"; echo "PWD = $PWD"; end
+
+    # Check to see if our CWD is inside the conda environment's root.
+    set _dir (string match "$__autoconda_old*" "$PWD")
+    if test "$autoconda_debug" = "yes"; echo "_dir = $_dir"; end
+
+    # If we're no longer inside the conda environment root deactivate it and return.
+    if test -z "$_dir" -a ! -e "$_source"
+
+      if test "$autoconda_debug" = "yes"; echo "Outside of conda env root"; end
+
+      conda deactivate
+
+      if test "$autoconda_announce" = "yes"
+        echo "Deactivated Virtual Enviroment ($__autoconda_new)"
+      end
+
+      set -e __autoconda_new
+      set -e __autoconda_old
+
+    # If we've switched into a different conda environment directory, deactivate the old and activate the new.
+    else if test -z "$_dir" -a -e "$_source"
+
+      if test "$autoconda_debug" = "yes"; echo "Switched to different conda env"; end
+
+      __autoconda_activate_from_file "$_source"
+
+      if test "$autoconda_announce" = "yes"
+        echo "Switched Virtual Environments ($__autoconda_old => $__autoconda_new)"
+      end
+    else
+      if test "$autoconda_debug" = "yes"; echo "Impossible"; end
+    end
+  else
+    if test "$autoconda_debug" = "yes"; echo "No _source and no CONDA_DEFAULT_ENV"; end
+  end
+end
+
+function __autoconda_activate_from_file -d "Activates a conda environment mentioned in a given environment.yml"
+  if test "$autoconda_debug" = "yes"; echo "Started __autoconda_activate_from_file"; end
+
+  set -l environment_yml $argv[1]
+  set -l prefix_line (cat $environment_yml | grep "^prefix:")
+  set -l prefix_split (string split -n " " $prefix_line)
+  set -l prefix_from_file $prefix_split[2]
+
+  if test "$autoconda_debug" = "yes"; echo "environment_yml = $environment_yml"; end
+  if test "$autoconda_debug" = "yes"; echo "prefix_line = $prefix_line"; end
+  if test "$autoconda_debug" = "yes"; echo "prefix_split = $prefix_split"; end
+  if test "$autoconda_debug" = "yes"; echo "prefix_from_file = $prefix_from_file"; end
+
+  conda activate $prefix_from_file
 end
 ################################################################################
